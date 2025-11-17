95c453f0a212f8f89a4696378983462381b754a5
studente <test@gmail.com>
2025-11-17 12:23:02 +0100

    Ultimo es in classe

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+public static void main(String[] args) {
+    Counter counter = new Counter();
 
+    // Create multiple threads to increment the counter
+    Thread t1 = new Thread(() -> counter.increment());
+    Thread t2 = new Thread(() -> counter.increment());
+    SynchronizedBlockExample example = new SynchronizedBlockExample();
+
+    // Create multiple threads to increment the counter
+    Thread t3 = new Thread(() -> example.increment());
+    Thread t4 = new Thread(() -> example.increment());
+
+    t1.start();
+    t2.start();
+
+    try {
+        t1.join();
+        t2.join();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("Final count: " + counter.getCount());
+        
     }
 }
